8ca1ed8a984d1e3d89bb3094721838a9d98fd40d
Ilja Erbis <iljaerbis@gmx.de>
2016-01-01 22:41:39 +0100

    Refactoring

--- src/main/java/com/ie/fbrss/fb/FbCollector.java
+++ src/main/java/com/ie/fbrss/fb/FbCollector.java
@@ -1,126 +1,131 @@
 package com.ie.fbrss.fb;
 
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.Date;
-import java.util.concurrent.ConcurrentHashMap;
-import java.util.concurrent.ConcurrentMap;
-
-import org.springframework.social.facebook.api.Comment;
-import org.springframework.social.facebook.api.Facebook;
-import org.springframework.social.facebook.api.PagedList;
-import org.springframework.social.facebook.api.PagingParameters;
-import org.springframework.social.facebook.api.Post;
+import com.ie.fbrss.data.FbPage;
+import com.ie.fbrss.data.RssContent;
+import org.springframework.social.facebook.api.*;
 import org.springframework.social.facebook.api.impl.FacebookTemplate;
 import org.springframework.stereotype.Component;
 import org.springframework.ui.Model;
 
-import com.ie.fbrss.data.FbPage;
-import com.ie.fbrss.data.RssContent;
+import java.util.Collection;
+import java.util.Collections;
+import java.util.Date;
+import java.util.List;
+import java.util.concurrent.ConcurrentHashMap;
+import java.util.concurrent.ConcurrentMap;
+import java.util.stream.Collectors;
 
 @Component
 public final class FbCollector {
 
-	private static final String FACEBOOK_GRAPH_URL = "https://graph.facebook.com/";
+    private static final String FACEBOOK_GRAPH_URL = "https://graph.facebook.com/";
 
-	private static final String FACEBOOK_URL = "https://www.facebook.com/";
+    private static final String FACEBOOK_URL = "https://www.facebook.com/";
 
-	private static final PagingParameters PAGING_PARAMETERS = new PagingParameters(5, 0, null, null);
+    private static final PagingParameters PAGING_PARAMETERS = new PagingParameters(5, 0, null, null);
 
-	private final Facebook facebook;
+    private final Facebook facebook;
 
-	private final ConcurrentMap<String, FbPage> fbSiteCache = new ConcurrentHashMap<>();
+    private final ConcurrentMap<String, FbPage> fbSiteCache = new ConcurrentHashMap<>();
 
-	private FbCollector() {
-		String fbAppId = System.getenv().get("spring.social.facebook.appId");
-		String fbAppSecret = System.getenv().get("spring.social.facebook.appSecret");
+    private FbCollector() {
+        String fbAppId = System.getenv()
+                .get("spring.social.facebook.appId");
+        String fbAppSecret = System.getenv()
+                .get("spring.social.facebook.appSecret");
 
-		if (fbAppId == null || fbAppSecret == null) {
-			fbAppId = System.getProperty("spring.social.facebook.appId");
-			fbAppSecret = System.getProperty("spring.social.facebook.appSecret");
-		}
+        if (fbAppId == null || fbAppSecret == null) {
+            fbAppId = System.getProperty("spring.social.facebook.appId");
+            fbAppSecret = System.getProperty("spring.social.facebook.appSecret");
+        }
 
-		if (fbAppId != null && fbAppSecret != null) {
-			facebook = connectToFacebook(fbAppId + "|" + fbAppSecret);
-		} else {
-			facebook = null;
-		}
-	}
+        if (fbAppId != null && fbAppSecret != null) {
+            facebook = connectToFacebook(fbAppId + "|" + fbAppSecret);
+        } else {
+            facebook = null;
+        }
+    }
 
-	public void collectData(final String fbUrl, final Model model) {
+    public void collectData(final String fbUrl, final Model model) {
 
-		if (facebook == null) {
-			final RssContent data = new RssContent();
-			data.setTitle("Could not authorize to Facebook!");
-			data.setUrl(fbUrl);
-			data.setSummary("");
-			data.setCreatedDate(new Date());
-			model.addAttribute("feedContent", Arrays.asList(data));
-			model.addAttribute("feedMetadata", data);
-			return;
-		}
+        if (facebook == null) {
+            final RssContent data = new RssContent();
+            data.setTitle("Could not authorize to Facebook!");
+            data.setUrl(fbUrl);
+            data.setSummary("");
+            data.setCreatedDate(new Date());
+            model.addAttribute("feedContent", Collections.singletonList(data));
+            model.addAttribute("feedMetadata", data);
+            return;
+        }
 
-		final FbPage fbPage = getId(fbUrl);
+        final FbPage fbPage = getId(fbUrl);
 
-		final RssContent feedMetadata = new RssContent();
-		feedMetadata.setTitle(fbPage.getName());
-		feedMetadata.setUrl(fbUrl);
+        final RssContent feedMetadata = new RssContent();
+        feedMetadata.setTitle(fbPage.getName());
+        feedMetadata.setUrl(fbUrl);
 
-		final ArrayList<RssContent> feedContent = new ArrayList<RssContent>();
+        final PagedList<Post> posts = facebook.feedOperations()
+                .getFeed(fbPage.getId(), PAGING_PARAMETERS);
 
-		final PagedList<Post> posts = facebook.feedOperations().getFeed(fbPage.getId(), PAGING_PARAMETERS);
-		for (final Post post : posts) {
-			feedContent.add(createPostEntry(post));
-			final PagedList<Comment> comments = facebook.commentOperations().getComments(post.getId(),
-					PAGING_PARAMETERS);
-			for (final Comment comment : comments) {
-				feedContent.add(createCommentContent(comment));
-			}
-		}
+        // Collect all posts:
+        List<RssContent> feedContent = posts.stream()
+                .map(this::createPostEntry)
+                .collect(Collectors.toList());
 
-		model.addAttribute("feedContent", feedContent);
-		model.addAttribute("feedMetadata", feedMetadata);
-	}
+        // Also collect all comments of each post:
+        posts.stream()
+                .map(post -> facebook.commentOperations()
+                        .getComments(post.getId(), PAGING_PARAMETERS))
+                .flatMap(Collection::stream)
+                .map(this::createCommentContent)
+                .forEachOrdered(feedContent::add);
 
-	private Facebook connectToFacebook(final String token) {
-		try {
-			final Facebook facebook = new FacebookTemplate(token);
-			if (facebook.isAuthorized()) {
-				return facebook;
-			} else {
-				return null;
-			}
-		} catch (final Exception ex) {
-			return null;
-		}
-	}
+        model.addAttribute("feedContent", feedContent);
+        model.addAttribute("feedMetadata", feedMetadata);
+    }
 
-	private RssContent createCommentContent(final Comment comment) {
-		final RssContent commentContent = new RssContent();
-		commentContent.setTitle("Comment from " + comment.getFrom().getName());
-		commentContent.setUrl(FACEBOOK_URL + comment.getId());
-		commentContent.setSummary(comment.getMessage());
-		commentContent.setCreatedDate(comment.getCreatedTime());
-		return commentContent;
-	}
+    private Facebook connectToFacebook(final String token) {
+        try {
+            final Facebook facebook = new FacebookTemplate(token);
+            if (facebook.isAuthorized()) {
+                return facebook;
+            } else {
+                return null;
+            }
+        } catch (final Exception ex) {
+            return null;
+        }
+    }
 
-	private RssContent createPostEntry(final Post post) {
-		final RssContent content = new RssContent();
-		content.setTitle(post.getStory() != null ? post.getStory() : post.getMessage());
-		content.setUrl(FACEBOOK_URL + post.getId());
-		final String summary = post.getMessage()
-				+ (post.getDescription() != null ? "\nLink description: " + post.getDescription() : "")
-				+ (post.getPicture() != null ? "\nPicture: " + post.getPicture() : "");
-		content.setSummary(summary);
-		content.setCreatedDate(post.getCreatedTime());
-		return content;
-	}
+    private RssContent createCommentContent(final Comment comment) {
+        final RssContent commentContent = new RssContent();
+        commentContent.setTitle("Comment from " + comment.getFrom()
+                .getName());
+        commentContent.setUrl(FACEBOOK_URL + comment.getId());
+        commentContent.setSummary(comment.getMessage());
+        commentContent.setCreatedDate(comment.getCreatedTime());
+        return commentContent;
+    }
 
-	private FbPage getId(final String fbUrl) {
-		return fbSiteCache.computeIfAbsent(fbUrl, this::retrieveFbPage);
-	}
+    private RssContent createPostEntry(final Post post) {
+        final RssContent content = new RssContent();
+        content.setTitle(post.getStory() != null ? post.getStory() : post.getMessage());
+        content.setUrl(FACEBOOK_URL + post.getId());
+        final String summary = post.getMessage()
+                + (post.getDescription() != null ? "\nLink description: " + post.getDescription() : "")
+                + (post.getPicture() != null ? "\nPicture: " + post.getPicture() : "");
+        content.setSummary(summary);
+        content.setCreatedDate(post.getCreatedTime());
+        return content;
+    }
 
-	private FbPage retrieveFbPage(final String fbUrl) {
-		return facebook.restOperations().getForObject(FACEBOOK_GRAPH_URL + "{https}" + fbUrl, FbPage.class, "https://");
-	}
+    private FbPage getId(final String fbUrl) {
+        return fbSiteCache.computeIfAbsent(fbUrl, this::retrieveFbPage);
+    }
+
+    private FbPage retrieveFbPage(final String fbUrl) {
+        return facebook.restOperations()
+                .getForObject(FACEBOOK_GRAPH_URL + "{https}" + fbUrl, FbPage.class, "https://");
+    }
 }
--- src/main/java/com/ie/fbrss/viewer/RssViewer.java
+++ src/main/java/com/ie/fbrss/viewer/RssViewer.java
@@ -1,71 +1,71 @@
 package com.ie.fbrss.viewer;
 
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Map;
-
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
-
-import org.springframework.web.servlet.view.feed.AbstractRssFeedView;
-
 import com.ie.fbrss.data.RssContent;
 import com.rometools.rome.feed.rss.Channel;
 import com.rometools.rome.feed.rss.Content;
 import com.rometools.rome.feed.rss.Item;
+import org.springframework.stereotype.Component;
+import org.springframework.web.servlet.view.feed.AbstractRssFeedView;
 
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
+
+@Component("rssViewer")
 public final class RssViewer extends AbstractRssFeedView {
 
-	/*
-	 * (non-Javadoc)
-	 *
-	 * @see org.springframework.web.servlet.view.feed.AbstractRssFeedView#
-	 * buildFeedItems(java.util.Map, javax.servlet.http.HttpServletRequest,
-	 * javax.servlet.http.HttpServletResponse)
-	 */
-	@Override
-	protected List<Item> buildFeedItems(final Map<String, Object> model, final HttpServletRequest request,
-			final HttpServletResponse response) {
+    private static Item mapToRss(RssContent element) {
+        final Item item = new Item();
 
-		@SuppressWarnings("unchecked")
-		final List<RssContent> elements = (List<RssContent>) model.get("feedContent");
-		final List<Item> items = new ArrayList<>(elements.size());
+        final Content bodyText = new Content();
+        bodyText.setValue(element.getSummary());
 
-		for (final RssContent element : elements) {
+        item.setContent(bodyText);
+        item.setTitle(element.getTitle());
+        item.setLink(element.getUrl());
+        item.setPubDate(element.getCreatedDate());
 
-			final Item item = new Item();
+        return item;
+    }
 
-			final Content bodyText = new Content();
-			bodyText.setValue(element.getSummary());
-			item.setContent(bodyText);
-			item.setTitle(element.getTitle());
-			item.setLink(element.getUrl());
-			item.setPubDate(element.getCreatedDate());
+    /*
+     * (non-Javadoc)
+     *
+     * @see org.springframework.web.servlet.view.feed.AbstractRssFeedView#
+     * buildFeedItems(java.util.Map, javax.servlet.http.HttpServletRequest,
+     * javax.servlet.http.HttpServletResponse)
+     */
+    @Override
+    protected List<Item> buildFeedItems(final Map<String, Object> model, final HttpServletRequest request,
+                                        final HttpServletResponse response) {
+        @SuppressWarnings("unchecked")
+        final List<RssContent> elements = (List<RssContent>) model.get("feedContent");
 
-			items.add(item);
-		}
+        return elements.stream()
+                .map(RssViewer::mapToRss)
+                .collect(Collectors.toList());
+    }
 
-		return items;
-	}
+    /*
+     * (non-Javadoc)
+     *
+     * @see org.springframework.web.servlet.view.feed.AbstractFeedView#
+     * buildFeedMetadata(java.util.Map, com.rometools.rome.feed.WireFeed,
+     * javax.servlet.http.HttpServletRequest)
+     */
+    @Override
+    protected void buildFeedMetadata(final Map<String, Object> model, final Channel feed,
+                                     final HttpServletRequest request) {
 
-	/*
-	 * (non-Javadoc)
-	 *
-	 * @see org.springframework.web.servlet.view.feed.AbstractFeedView#
-	 * buildFeedMetadata(java.util.Map, com.rometools.rome.feed.WireFeed,
-	 * javax.servlet.http.HttpServletRequest)
-	 */
-	@Override
-	protected void buildFeedMetadata(final Map<String, Object> model, final Channel feed,
-			final HttpServletRequest request) {
+        final RssContent feedMetadata = (RssContent) model.get("feedMetadata");
 
-		final RssContent feedMetadata = (RssContent) model.get("feedMetadata");
+        feed.setTitle(feedMetadata.getTitle());
+        feed.setDescription(feedMetadata.getTitle());
+        feed.setLink(feedMetadata.getUrl());
 
-		feed.setTitle(feedMetadata.getTitle());
-		feed.setDescription(feedMetadata.getTitle());
-		feed.setLink(feedMetadata.getUrl());
-
-		super.buildFeedMetadata(model, feed, request);
-	}
+        super.buildFeedMetadata(model, feed, request);
+    }
 
 }
